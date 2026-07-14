162fe0dfb4b92e1ee3b4536095d6b6a08039b705
uswiii <uswa@raresense.so>
2026-07-15 04:27:39 +0500

    style(shopify): show tester callout beside button from md breakpoint

--- src/components/shopify/ShopifyTesterCallout.tsx
+++ src/components/shopify/ShopifyTesterCallout.tsx
@@ -63,8 +63,8 @@ function TesterCallout({ onDismiss }: { onDismiss: () => void }) {
       className={cn(
         // Mobile / narrow: in-flow card above the button, arrow pointing down.
         'relative z-40 mb-3 w-full rounded-xl border border-border bg-background p-4 text-left shadow-xl',
-        // lg+: floats to the right of the button, arrow pointing left at it.
-        'lg:absolute lg:left-full lg:top-1/2 lg:mb-0 lg:ml-4 lg:w-72 lg:-translate-y-1/2'
+        // md+: floats to the right of the button, arrow pointing left at it.
+        'md:absolute md:left-full md:top-1/2 md:mb-0 md:ml-4 md:w-72 md:-translate-y-1/2'
       )}
     >
       <button
@@ -97,12 +97,12 @@ function TesterCallout({ onDismiss }: { onDismiss: () => void }) {
       {/* Arrow pointing down at the button (mobile placement) */}
       <span
         aria-hidden="true"
-        className="absolute -bottom-[7px] left-1/2 h-3.5 w-3.5 -translate-x-1/2 rotate-45 border-b border-r border-border bg-background lg:hidden"
+        className="absolute -bottom-[7px] left-1/2 h-3.5 w-3.5 -translate-x-1/2 rotate-45 border-b border-r border-border bg-background md:hidden"
       />
       {/* Arrow pointing left at the button (desktop placement) */}
       <span
         aria-hidden="true"
-        className="absolute -left-[7px] top-1/2 hidden h-3.5 w-3.5 -translate-y-1/2 rotate-45 border-b border-l border-border bg-background lg:block"
+        className="absolute -left-[7px] top-1/2 hidden h-3.5 w-3.5 -translate-y-1/2 rotate-45 border-b border-l border-border bg-background md:block"
       />
     </div>
   );
